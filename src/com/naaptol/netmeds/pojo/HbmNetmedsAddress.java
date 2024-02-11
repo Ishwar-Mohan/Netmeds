aaac7a5aea80eb3df0f75185f16f3744ad9f5104
Ishwar Mohan <107830130+Ishwar-Mohan@users.noreply.github.com>
2024-02-11 12:50:32 +0530

    commit

--- src/com/naaptol/netmeds/pojo/HbmNetmedsAddress.java
+++ src/com/naaptol/netmeds/pojo/HbmNetmedsAddress.java
@@ -6,8 +6,8 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 
 
 /** @author Hibernate CodeGenerator */
-public class HbmNetmedsAddress implements Serializable {
-
+public class HbmNetmedsAddress implements Serializable
+{
     /** identifier field */
     private Long addressId;
 
@@ -45,7 +45,8 @@ public class HbmNetmedsAddress implements Serializable {
     private String isActive;
 
     /** full constructor */
-    public HbmNetmedsAddress(String address, String city, String state, String pincode, String landmark, HbmNetmedsCustomer customer, String createdBy, Date createdDate, String modifiedBy, Date modifiedDate, String isActive) {
+    public HbmNetmedsAddress(String address, String city, String state, String pincode, String landmark, HbmNetmedsCustomer customer, String createdBy, Date createdDate, String modifiedBy, Date modifiedDate, String isActive)
+    {
         this.address = address;
         this.city = city;
         this.state = state;
@@ -60,86 +61,105 @@ public class HbmNetmedsAddress implements Serializable {
     }
 
     /** default constructor */
-    public HbmNetmedsAddress() {
-    }
+    public HbmNetmedsAddress() {}
 
-    public Long getAddressId() {
+    public Long getAddressId() 
+    {
         return this.addressId;
     }
 
-    public void setAddressId(Long addressId) {
+    public void setAddressId(Long addressId) 
+    {
         this.addressId = addressId;
     }
 
-    public String getAddress() {
+    public String getAddress() 
+    {
         return this.address;
     }
 
-    public void setAddress(String address) {
+    public void setAddress(String address) 
+    {
         this.address = address;
     }
 
-    public String getCity() {
+    public String getCity() 
+    {
         return this.city;
     }
 
-    public void setCity(String city) {
+    public void setCity(String city) 
+    {
         this.city = city;
     }
 
-    public String getState() {
+    public String getState() 
+    {
         return this.state;
     }
 
-    public void setState(String state) {
+    public void setState(String state)
+    {
         this.state = state;
     }
 
-    public String getPincode() {
+    public String getPincode() 
+    {
         return this.pincode;
     }
 
-    public void setPincode(String pincode) {
+    public void setPincode(String pincode)
+    {
         this.pincode = pincode;
     }
 
-    public String getLandmark() {
+    public String getLandmark() 
+    {
         return this.landmark;
     }
 
-    public void setLandmark(String landmark) {
+    public void setLandmark(String landmark) 
+    {
         this.landmark = landmark;
     }
     
-    public HbmNetmedsCustomer getCustomer() {
+    public HbmNetmedsCustomer getCustomer() 
+    {
         return customer;
     }
 
-    public void setCustomer(HbmNetmedsCustomer customer) {
+    public void setCustomer(HbmNetmedsCustomer customer) 
+    {
         this.customer = customer;
     }
     
-    public String getCreatedBy() {
+    public String getCreatedBy() 
+    {
         return this.createdBy;
     }
 
-    public void setCreatedBy(String createdBy) {
+    public void setCreatedBy(String createdBy) 
+    {
         this.createdBy = createdBy;
     }
 
-    public Date getCreatedDate() {
+    public Date getCreatedDate() 
+    {
         return this.createdDate;
     }
 
-    public void setCreatedDate(Date createdDate) {
+    public void setCreatedDate(Date createdDate) 
+    {
         this.createdDate = createdDate;
     }
 
-    public String getModifiedBy() {
+    public String getModifiedBy() 
+    {
         return this.modifiedBy;
     }
 
-    public void setModifiedBy(String modifiedBy) {
+    public void setModifiedBy(String modifiedBy)
+    {
         this.modifiedBy = modifiedBy;
     }
 
@@ -147,22 +167,25 @@ public class HbmNetmedsAddress implements Serializable {
         return this.modifiedDate;
     }
 
-    public void setModifiedDate(Date modifiedDate) {
+    public void setModifiedDate(Date modifiedDate) 
+    {
         this.modifiedDate = modifiedDate;
     }
 
-    public String getIsActive() {
+    public String getIsActive() 
+    {
         return this.isActive;
     }
 
-    public void setIsActive(String isActive) {
+    public void setIsActive(String isActive)
+    {
         this.isActive = isActive;
     }
 
-    public String toString() {
+    public String toString() 
+    {
         return new ToStringBuilder(this)
             .append("addressId", getAddressId())
             .toString();
     }
-
 }
